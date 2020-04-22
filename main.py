15f4ba10b0629e7f01cdf349c4198a8568472f0e
janza <franciscojanza001@gmail.com>
2020-04-22 12:45:17 -0300

    pongo imagenes de botones

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import sys, pygame
 from pygame.locals import *
+from tkinter import *
+
 
 WIDTH = 960
 HEIGHT = 480
 
+#--------------------------------------------------
+
+#--------------------------------------------------
+
+#--------------------------------------------------
+
+#--------------------------------------------------
+
+#--------------------------------------------------
 
 
 #--------------------------------------------------
@@ -18,16 +29,34 @@ def load_image(filename, transparent=False):
         return  image
 #--------------------------------------------------
 def main():
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("pruebas pygame")
 
-    background_image = load_image('data/fondo.jpg')
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    pygame.display.set_caption("freestyle")
+
+    button_image = load_image('data/button.png')
+
+    background_image = load_image('data/fondo.png')
 
     while True:
+
         for eventos in pygame.event.get():
+
+
             if eventos.type == QUIT:
-                sys.exit(0)
+                 sys.exit(0)
+
+
+
+
+
         screen.blit(background_image,(0,0))
+        screen.blit(button_image, (50, 330))
+        screen.blit(button_image, (610, 330))
+
+
+
         pygame.display.flip()
     return 0
 #--------------------------------------------------
